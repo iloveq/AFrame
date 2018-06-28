9db4cb9a8df126906c7fd28d11d3b4f44397a45e
woaigmz <woaigmz@gmail.com>
2018-06-28 15:31:36 +0800

    add adapter

--- app/src/main/java/com/woaiqw/aframe/view/MainActivity.java
+++ app/src/main/java/com/woaiqw/aframe/view/MainActivity.java
@@ -2,27 +2,20 @@ package com.woaiqw.aframe.view;
 
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 
 import com.woaiqw.aframe.R;
+import com.woaiqw.aframe.view.widget.BorderDividerItemDecoration;
 import com.woaiqw.base.common.BaseActivity;
 import com.woaiqw.base.utils.ToastUtil;
 
 import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 public class MainActivity extends BaseActivity {
 
-    @BindView(R.id.tv0)
-    TextView tv0;
-    @BindView(R.id.tv1)
-    TextView tv1;
-    @BindView(R.id.tv2)
-    TextView tv2;
-    @BindView(R.id.tv3)
-    TextView tv3;
+    @BindView(R.id.rv)
+    RecyclerView rv;
 
     @Override
     protected int getLayoutId() {
@@ -31,29 +24,10 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void afterCreate(Bundle savedInstanceState) {
-        tv0.setText("空数据");
-        tv1.setText("网络错误");
-        tv2.setText("重试");
-        tv3.setText("加载中");
-    }
-
-
-    @OnClick({R.id.tv0, R.id.tv1, R.id.tv2, R.id.tv3})
-    public void onViewClicked(View view) {
-        switch (view.getId()) {
-            case R.id.tv0:
-                showEmptyView();
-                break;
-            case R.id.tv1:
-                showErrorView();
-                break;
-            case R.id.tv2:
-                showNoNetworkRetryView();
-                break;
-            case R.id.tv3:
-                showLoadingView();
-                break;
-        }
+        BorderDividerItemDecoration itemDecoration = new BorderDividerItemDecoration(this.getResources().getDimensionPixelOffset(R.dimen.border_divider_height), this.getResources().getDimensionPixelOffset(R.dimen.border_padding_spans));
+        StaggeredGridLayoutManager staggeredGridLayoutManager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+        rv.setLayoutManager(staggeredGridLayoutManager);
+        rv.addItemDecoration(itemDecoration);
     }
 
     @Override
