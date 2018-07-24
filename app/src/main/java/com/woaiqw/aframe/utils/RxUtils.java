9cce311c740bbcee927d3a58cafbab83acef0aea
woaigmz <woaigmz@gmail.com>
2018-07-24 21:25:06 +0800

    clean code

--- app/src/main/java/com/woaiqw/aframe/utils/RxUtils.java
+++ app/src/main/java/com/woaiqw/aframe/utils/RxUtils.java
@@ -1,11 +1,8 @@
 package com.woaiqw.aframe.utils;
 
 
-
 import com.woaiqw.aframe.bean.BaseResult;
 
-import io.reactivex.Observable;
-import io.reactivex.ObservableSource;
 import io.reactivex.ObservableTransformer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.functions.Function;
@@ -22,22 +19,12 @@ public final class RxUtils {
 
 
     public static <T> ObservableTransformer<T, T> io2main() {
-        return new ObservableTransformer<T, T>() {
-            @Override
-            public ObservableSource<T> apply(Observable<T> upstream) {
-                return upstream.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());
-            }
-        };
+        return upstream -> upstream.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());
     }
 
 
     public static <T> ObservableTransformer<BaseResult<T>, T> transform() {
-        return new ObservableTransformer<BaseResult<T>, T>() {
-            @Override
-            public ObservableSource<T> apply(Observable<BaseResult<T>> upstream) {
-                return upstream.map(new GsonFactoryFunc<T>()).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).unsubscribeOn(Schedulers.io());
-            }
-        };
+        return upstream -> upstream.map(new GsonFactoryFunc<>()).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).unsubscribeOn(Schedulers.io());
     }
 
     public static class GsonFactoryFunc<T> implements Function<BaseResult<T>, T> {
